8253071ab7b920d9b611569377afc114f25a8021
Aakash Khanal <aakashkhanal015@gmail.com>
2022-01-30 09:30:52 +0545

    path animated afted nodes visited animation

--- src/hooks/useAnimations.ts
+++ src/hooks/useAnimations.ts
@@ -1,14 +1,17 @@
 const useAnimations = () => {
+  let count = 0;
+
   const visitedNodeAni = (visitedNodes: GridNode[], pathNodes: GridNode[]) => {
     visitedNodes?.forEach((node, i) => {
       const { row, col } = node;
-      if (node.isFinish) animateShortestPath(pathNodes);
       setTimeout(() => {
         if (!node.isStart && !node.isFinish && !node.isPath) {
           document
             .getElementById(`node-${col}-${row}`)
             ?.classList.add("visited-node");
         }
+        count = count + 1;
+        if (count === visitedNodes.length) animateShortestPath(pathNodes);
       }, 10 * i);
     });
   };
